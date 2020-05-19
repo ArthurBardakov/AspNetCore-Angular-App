6c5d5e265ba2bbbabfed16dbf4bd02d8d819dfba
Arthur <artleks23@gmail.com>
2020-05-19 11:54:40 +0300

    declarations syntax update

--- ResourceAuthServer/Infrastructure/Seeds/IdentityDbInitilizer.cs
+++ ResourceAuthServer/Infrastructure/Seeds/IdentityDbInitilizer.cs
@@ -30,17 +30,15 @@ namespace ResourceAuthServer.Infrastructure.Seeds
 
         public async Task StartAsync(CancellationToken cancellationToken)
         {
-            using (var scope = serviceProvider.CreateScope())
-            {
-                IServiceProvider sp = scope.ServiceProvider;
-                var env = sp.GetRequiredService<IWebHostEnvironment>();
-                currentPath = env.ContentRootPath;
-                context = sp.GetRequiredService<HospitalContext>();
-                await context.Database.MigrateAsync();
-                await CreateRoles(sp);
-                await CreateUsers(sp);
-                await context.SaveChangesAsync();
-            }
+            using var scope = serviceProvider.CreateScope();
+            IServiceProvider sp = scope.ServiceProvider;
+            var env = sp.GetRequiredService<IWebHostEnvironment>();
+            currentPath = env.ContentRootPath;
+            context = sp.GetRequiredService<HospitalContext>();
+            await context.Database.MigrateAsync();
+            await CreateRoles(sp);
+            await CreateUsers(sp);
+            await context.SaveChangesAsync();
         }
 
         private async Task CreateRoles(IServiceProvider provider)
@@ -87,47 +85,39 @@ namespace ResourceAuthServer.Infrastructure.Seeds
 
         private async IAsyncEnumerable<ApplicationUser> generateTestUsers()
         {
-            using (var fs = File.OpenRead(currentPath + "\\test-users.csv"))
+            using var fs = File.OpenRead(currentPath + "\\test-users.csv");
+            using var sr = new StreamReader(fs);
+            string line = await sr.ReadLineAsync();
+            string[] userProps;
+            Doctor doctor;
+            string[] specializations = (await loadDoctorSpecializations()).ToArray();
+            Random rand = new Random();
+
+            for (int i = 0; (line = await sr.ReadLineAsync()) != null; i++)
             {
-                using (var sr = new StreamReader(fs))
+                userProps = line.Split(',');
+
+                if (i % 2 == 0)
                 {
-                    string line = await sr.ReadLineAsync();
-                    string[] userProps;
-                    Doctor doctor;
-                    string[] specializations = (await loadDoctorSpecializations()).ToArray();
-                    Random rand = new Random();
-
-                    for (int i = 0; (line = await sr.ReadLineAsync()) != null; i++)
-                    {
-                        userProps = line.Split(',');
-
-                        if (i % 2 == 0)
-                        {
-                            yield return createUser<Patient>(userProps, rand);
-                        }
-                        else
-                        {
-                            doctor = (Doctor)createUser<Doctor>(userProps, rand);
-                            doctor.YearsOfExperience = rand.Next(2, 40);
-                            doctor.Specialization = specializations[rand.Next(0, specializations.Length - 1)];
-                            yield return doctor;
-                        }
-                    }
+                    yield return createUser<Patient>(userProps, rand);
+                }
+                else
+                {
+                    doctor = (Doctor)createUser<Doctor>(userProps, rand);
+                    doctor.YearsOfExperience = rand.Next(2, 40);
+                    doctor.Specialization = specializations[rand.Next(0, specializations.Length - 1)];
+                    yield return doctor;
                 }
             }
         }
 
         private async Task<IEnumerable<string>> loadDoctorSpecializations()
         {
-            using (var fs = File.OpenRead(currentPath + "\\doctor-specializations.json"))
-            {
-                using (var sr = new StreamReader(fs))
-                {
-                    var jsonStr = await sr.ReadToEndAsync();
-                    return JsonConvert.DeserializeObject<JToken>(jsonStr)
-                                ["specializations"].Values<string>();
-                }
-            }
+            using var fs = File.OpenRead(currentPath + "\\doctor-specializations.json");
+            using var sr = new StreamReader(fs);
+            var jsonStr = await sr.ReadToEndAsync();
+            return JsonConvert.DeserializeObject<JToken>(jsonStr)
+                        ["specializations"].Values<string>();
         }
 
         private TUser createUser<TUser>(
